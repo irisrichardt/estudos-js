2128d2a8ded21e1478905d07f897eb875286c9be
Íris <iris.balk04@gmail.com>
2024-10-09 00:37:43 -0300

    Melhorias na hora de adicionar um novo item

--- js/adicionarItem.js
+++ js/adicionarItem.js
@@ -7,9 +7,14 @@ const listaDeCompras = document.getElementById("lista-de-compras");
 export function adicionarItem(e) {
   e.preventDefault();
 
+  if (item.value.trim() === "") {
+    alert("Por favor, digite o nome do item!");
+    item.value = "";
+    return;
+  }
+
   const itemDaLista = criarItemDaLista(item.value);
-
   listaDeCompras.appendChild(itemDaLista);
-
   verificarListaVazia(listaDeCompras);
+  item.value = "";
 }
